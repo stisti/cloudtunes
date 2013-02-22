ab943a5ad0ee4666c4076655dd2f04a5e61cfb04
Sami Tikka <sjtikka@gmail.com>
2013-02-22 12:03:19 +0200

    Re-arranging ticketing

---
+++ get_ticket.js
@@ -0,0 +1,5 @@
+var fsioticket = require("./ticket");
+
+fsioticket(8124, function (ticket) {
+	console.log(ticket);
+});
--- ticket.js
+++ ticket.js
@@ -1,34 +1,54 @@
-// Create a static web server
-var port = 8124;
-var child_process = require("child_process");
-var ns = require("node-static");
-var file = new(ns.Server)("./web", {cache: false});
-require("http").createServer(function (request, response) {
-    if (request.method == "GET") {
-        request.addListener("end", function () {
-            file.serve(request, response);
-        });
-    } else if (request.method == "POST") {
-        var data = "";
-        request.setEncoding("utf8");
-        request.addListener("data", function (chunk) {
-            data += chunk;
-        });
-        request.addListener("end", function () {
-            response.statusCode = 200;
-            response.end();
-            proceed(data);
-        });
-    }
-}).listen(port, function () {
-    child_process.exec("open http://127.0.0.1:" + port + "/index.html",
-                       function (err, stdout, stderr) {
-        if (err) {
-            console.error("Failed to launch browser:", stderr);
-        }
-    });
-});
+// Listen to @port and launch browser to obtain a ticket.
+// Finally call @callback with the ticket.
+function FsioTicket(port, callback) {
+    var child_process = require("child_process");
 
-function proceed(ticket) {
-    console.log("Got the ticket", ticket);
+    // Create a static web server
+    var ns = require("node-static");
+    var file = new(ns.Server)("./web", {cache: false});
+
+    // Static web server serves a web site to a browser that
+    // logs in and acquires an FSIO ticket.
+    // The JavsScript in the browser then POSTs the ticket
+    // back to this web server.
+    require("http").createServer(function (request, response) {
+        if (request.method == "GET") {
+            // Serving the web site for the browser
+            request.addListener("end", function () {
+                file.serve(request, response);
+            });
+        } else if (request.method == "POST") {
+            // Receiving the ticket from the browser
+            var data = "";
+            request.setEncoding("utf8");
+            request.addListener("data", function (chunk) {
+                data += chunk;
+            });
+            request.addListener("end", function () {
+                response.statusCode = 200;
+                response.end();
+                // Deliver the ticket
+                callback(data);
+            });
+        }
+    }).listen(port, function () {
+        var urlopener = null;
+        if (process.platform == "darwin") {
+            urlopener = "open";
+        } else if (process.platform == "linux") {
+            urlopener = "xdg-open";
+        } else if (process.platform == "win32") {
+            urlopener = "start"
+        } else {
+            throw new Error("Don't know how to open web pages in platform " + process.platform);
+        }
+        child_process.exec(urlopener + " http://127.0.0.1:" + port + "/index.html",
+                           function (err, stdout, stderr) {
+            if (err) {
+                console.error("Failed to launch browser:", stderr);
+            }
+        });
+    });
 }
+
+module.exports = FsioTicket;
